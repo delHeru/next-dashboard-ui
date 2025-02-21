574aa8b8e04873355c9c1db53b05247ca0215ae4
heru <thedreamer.19@gmail.com>
2025-02-22 00:06:10 +0700

    sampe announc

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -118,7 +118,7 @@ const menuItems = [
 
 const Menu = () => {
   return (
-    <div className=''>
+    <div className='mt-4 text-sm'>
       {menuItems.map((item) => (
         <div className="flex flex-col gap-2" key={item.title}>
           <span className="hidden lg:block text-gray-400 font-light my-4">{item.title}</span>
